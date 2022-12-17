0e3189b802ea76926c40d653538bf1cc4f934a87
shashankjs2002 <shashankjs2002@gmail.com>
2022-12-17 19:17:30 +0530

    v1

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,9 +4,9 @@ import MeriFotu from './assets/fotu2.png'
 
 const Home = () => {
   // document.getElementById('home').style.height = window.innerHeight
-  document.getElementById('home').style.height = `${window.innerHeight}px`
+  // document.getElementById('home').style.height = `${window.innerHeight}px`
   return (
-    <div className='' id='home'>
+    <div className='' id='home' style={{height: `${window.innerHeight}px`}}>
     
         <div className="name">
           <div className="hi">
